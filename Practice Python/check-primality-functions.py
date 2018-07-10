c66951842374de0729b496d957685f63513d4ab5
Teozard <teozard@gmail.com>
2018-07-10 17:29:42 +0300

    check primality functions

---
+++ Practice Python/check-primality-functions.py
@@ -0,0 +1,24 @@
+def get_number(prompt):
+    return int(input(prompt))
+
+
+def check_num(num):
+    if num == 1:
+        prime = False
+    elif num == 2:
+        prime = True
+    else:
+        prime = True
+        for check_number in range(2, int((num / 2) + 1)):
+            if num % check_number == 0:
+                prime = False
+                break
+    if prime:
+        descriptor = ""
+    else:
+        descriptor = "not "
+
+    print(num, " is ", descriptor, "prime.", sep="", end="\n\n")
+
+
+check_num(get_number("Enter a number to check. Ctl-C to exit."))
