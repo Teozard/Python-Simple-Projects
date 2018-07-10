274a7fe1cee5f35a9e52fb28f572b3f3ec0d95e3
Vasily.Romashin <teozad@gmail.com>
2018-07-10 12:57:37 +0300

    list-overlap

--- Practice Python/Divisors.py
+++ Practice Python/Divisors.py
@@ -1,16 +1,7 @@
-'''
-
-Create a program that asks the user for a number and then prints out a
-list of all the divisors of that number.
-(If you don’t know what a divisor is, it is a number that divides evenly into another number.
-For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
-
-'''
-
-x = list(range(2, 11))
-print(x)
-user = int(input("Submit your number"))
-
+num = int(input("Submit your number: "))
+x = list(range(1, num + 1))
+new_list = []
 for elem in x:
-    if elem % user == 0:
-        print(elem)
+    if elem % num == 0:
+        new_list.append(elem)
+print(new_list)
---
+++ Practice Python/list-overlap.py
@@ -0,0 +1,29 @@
+from random import randint
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# gen list 1
+list_1 = []
+for elem in range(0, 9):
+    elem = randint(0, 9)
+    list_1.append(elem)
+
+# gen list 2
+list_2 = []
+for elem in range(0, 9):
+    elem = randint(0, 9)
+    list_2.append(elem)
+
+print(list_1)
+print(list_2)
+
+ov_list = []
+for i in list_1:
+    if i in list_2:
+        ov_list.append(i)
+
+print(ov_list)
+print([aa for aa in list_1 if aa in list_2])
+
+
