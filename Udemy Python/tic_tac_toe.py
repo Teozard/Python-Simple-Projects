07f7303b802f4eed2f6263cf064b8385a781f967
Teozard <teozard@gmail.com>
2018-07-17 17:54:24 +0300

    ttt game

---
+++ Udemy Python/tic_tac_toe.py
@@ -0,0 +1,16 @@
+def display_board(board):
+    print(f'\
+{board[7]} {board[8]} {board[9]}\n\
+{board[4]} {board[5]} {board[6]}\n\
+{board[1]} {board[2]} {board[3]}\n')
+
+
+def player_input():
+	while player1 != 'X' or player1 != 'X':
+		player1 = input("Please pick a marker 'X' or 'O'")
+	print(player1)
+
+
+test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+display_board(test_board)
+player_input()
